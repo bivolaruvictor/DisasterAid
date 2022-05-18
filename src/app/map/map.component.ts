8a529bde7d0ede3bf2fde006e23ca63f19113ad3
bivolaruvictor <bivolaru.victor@gmail.com>
2022-05-18 13:45:00 +0000

    draw_on_map

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Loader } from '@googlemaps/js-api-loader';
 import data from '../../../auth_config.json';
+
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
@@ -8,40 +9,101 @@ import data from '../../../auth_config.json';
 })
 export class MapComponent implements OnInit {
   title = 'map'
+  
+  icons: Record<string, { icon: string }> = {
+    blood: {
+      icon: '../../assets/images/blood.png',
+    }};
+
   constructor() { }
 
   async ngOnInit(): Promise<void> {
+  
     let loader = new Loader({
-      apiKey: data.APIKEY
+      apiKey: data.APIKEY,
+      libraries: ['drawing', 'geometry', 'places', 'visualization']
     })
 
-    let center =  {lat: 44.439663, lng: 26.096306}
+    let center = {lat: 44.439663, lng: 26.096306}
+
     let mapElement = document.getElementById("map") as HTMLElement;
 
     const map = await loader.load().then(() => {
       return new google.maps.Map(mapElement, {
-        center: {lat: 44.439663, lng: 26.096306}, 
+        center: center, 
         zoom: 10
       })
     })
 
-    const marker = new google.maps.Marker({
-      position: center,
-      map,
-      title: "Click to zoom",
+    const drawingManager = new google.maps.drawing.DrawingManager({
+      drawingMode: google.maps.drawing.OverlayType.MARKER,
+      drawingControl: true,
+      drawingControlOptions: {
+        position: google.maps.ControlPosition.TOP_CENTER,
+        drawingModes: [
+          google.maps.drawing.OverlayType.MARKER,
+          google.maps.drawing.OverlayType.CIRCLE
+        ],
+      },
+      markerOptions: {
+        position: center,
+        map,
+        draggable: true,
+        animation: google.maps.Animation.DROP,
+        icon: this.icons['blood'].icon,
+      },
+      circleOptions: {
+        strokeColor: "#FF0000",
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: "#FF0000",
+        fillOpacity: 0.35,
+        map,
+        center: center,
+        radius: 100,
+      },
     });
+
+    drawingManager.setMap(map);
   
     map.addListener("click", (e: any) => {
-      this.placeMarkerAndPanTo(e.latLng, map);
+      this.placeMarker(e.latLng, map);
+    });
+
+
+    new google.maps.Circle({
+      strokeColor: "#FF0000",
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: "#FF0000",
+      fillOpacity: 0.35,
+      map,
+      center: center,
+      radius: 100,
     });
   }
 
-  placeMarkerAndPanTo(latLng: google.maps.LatLng, map: google.maps.Map) {
-    new google.maps.Marker({
-      position: latLng,
-      map: map,
+  placeMarker(latLng: google.maps.LatLng, map: google.maps.Map) {
+
+    const infowindow = new google.maps.InfoWindow({
+      content: 'Blood Donnor Needed',
     });
-    map.panTo(latLng);
+
+    let marker = new google.maps.Marker({
+      position: latLng,
+      map,
+      draggable: true,
+      animation: google.maps.Animation.DROP,
+      icon: this.icons['blood'].icon,
+    });
+
+    marker.addListener("click", () => {
+      infowindow.open({
+        anchor: marker,
+        map,
+        shouldFocus: false,
+      });
+    });
   }
   
 }
