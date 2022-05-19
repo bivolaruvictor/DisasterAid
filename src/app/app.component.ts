e10dd7332b5bd30424a1ea916dc56a9e8f1df56d
Andrei Dumitrescu <maxandyx13@gmail.com>
2022-05-19 14:38:58 +0000

    get_user_data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,13 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
 import { DOCUMENT } from '@angular/common';
 import {AppAuthButtonComponent} from '../app/app-auth-button/app-auth-button.component'
 import { AppRoutingModule } from './app-routing.module';
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore, doc, getDoc } from "firebase/firestore";
 import data from '../../auth_config.json';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'DisasterAid';
-  constructor(@Inject(DOCUMENT) public document: Document, public auth: AuthService) {}
-
-  
-}
+import { async } from '@firebase/util';
 
 const firebaseConfig = {
   apiKey: data.apiKey,
@@ -34,3 +23,12 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const db = getFirestore(app);
 
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'DisasterAid';
+  constructor(@Inject(DOCUMENT) public document: Document, public auth: AuthService) {}
+}
--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,11 +1,62 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
 import { DOCUMENT } from '@angular/common';
 import { MapComponent } from '../map/map.component';
+import { initializeApp } from "firebase/app";
+import { getFirestore, doc, getDoc } from "firebase/firestore";
+import data from '../../../auth_config.json';
+
+const firebaseConfig = {
+  apiKey: data.apiKey,
+  authDomain: data.authDomain,
+  projectId: data.projectId,
+  storageBucket: data.storageBucket,
+  messagingSenderId: data.messagingSenderId,
+  appId: data.appId,
+  measurementId: data.measurementId
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html'
 })
-export class UserProfileComponent {
+export class UserProfileComponent implements OnInit{
+  profileJson: any;
+  name : any;
+  username : any;
+  user : any;
+  bloodType : any;
+  email : any;
+  phone : any;
+  address : any;
+  
   constructor(@Inject(DOCUMENT) public document: Document, public auth: AuthService) {}
+    ngOnInit(): void {
+      this.auth.isAuthenticated$.subscribe(
+        (profile) => (this.profileJson = profile),
+      );
+      console.log(this.profileJson);
+      // let user = this.auth.user$;
+      // console.log(loginInfo);
+      this.auth.user$.subscribe(
+        (profile) => (this.user = profile),
+      );
+      // console.log(this.user.name); 
+      // const docRef = doc(db, "users", ); 
+      // const docSnap = await getDoc(docRef);
+      
+      // if(docSnap.exists()) {
+  
+      // } else {
+        
+      // }
+    }
+    
+  
 }
+
+
+
